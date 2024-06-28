a9e84a508620ca8b72c1608b9d7c3b9a5ce8fad8
Halcyon <hashem_elmaghrabi@yahoo.com>
2024-06-28 09:50:35 -0400

    Added JavaScript for blog form to form.js

---
+++ Assets/js/form.js
@@ -0,0 +1,32 @@
+// tutor helped design the following code
+const username = document.getElementById("username");
+const title = document.getElementById("title");
+const content = document.getElementById("content");
+const form = document.getElementById("blog-form");
+
+form.addEventListener("submit", function (event) {
+  // prevent the page from refreshing when a submit event is triggered
+  event.preventDefault();
+
+  const blogpost = {
+    username: username.value,
+    title: title.value,
+    content: content.value,
+  };
+
+//   trying to retireve the blogposts from LS
+  let blogposts = JSON.parse(localStorage.getItem("blogposts"));
+
+/**
+ *    if they don't exist, give them a value of an empty array
+ *  store that array in ls
+ *  add the new blog post to the array of posts
+ *  overwrite the array in LS with the new array containing the latest blog post
+ */
+  if (blogposts === null) {
+    blogposts = [];
+  }
+
+  blogposts.push(blogpost)
+  localStorage.setItem("blogposts", JSON.stringify(blogposts));
+});
